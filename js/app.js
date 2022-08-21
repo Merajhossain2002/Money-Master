f2d2bc25b20cb8810438e3fefd1f51f7a99a1188
Meraj Hossain <mdmaraj1500@gmail.com>
2022-08-21 09:16:10 +0600

    calculate button event complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+document.getElementById("calculate").addEventListener("click", function () {
+  const foodExpense = inputValueToIntegerById("food");
+  const rentExpenses = inputValueToIntegerById("rent");
+  const clothesExpenses = inputValueToIntegerById("clothes");
 
+  const totalExpense = foodExpense + rentExpenses + clothesExpenses;
+
+  const totalExpenseElement = document.getElementById("total-expense");
+  totalExpenseElement.innerText = totalExpense;
+
+  const income = inputValueToIntegerById("income");
+  const totalExpenseAmount = textValueToIntegerById("total-expense");
+
+  const balance = income - totalExpenseAmount;
+
+  const balanceElement = document.getElementById("balance");
+  balanceElement.innerText = balance;
+});
 
 
--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
-function convertToIntegerById(elementId) {
+function inputValueToIntegerById(elementId) {
   const getElement = document.getElementById(elementId);
   const getStringvalue = getElement.value;
   const convertedValue = parseInt(getStringvalue);
+  return convertedValue;
+}
+
+function textValueToIntegerById(elementId) {
+  const getElement = document.getElementById(elementId);
+  const getStringvalue = getElement.innerText;
+  const convertedValue = parseInt(getStringvalue);
+  return convertedValue;
 }
